381a96e247517fadf9b69b100d2dcd3526f41fe9
Ritik208 <94600316+Ritik208@users.noreply.github.com>
2024-08-11 14:06:49 +0530

    Delete frontend directory

--- frontend/src/elements/Home.jsx
+++
@@ -1,9 +0,0 @@
-export function Home() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold ">
-                Welcome, Select Compiler/Arena from navbar
-            </h1>
-        </div>
-    );
-}
